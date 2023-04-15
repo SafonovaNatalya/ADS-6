0fe793d80bc9c6d883bb39d17c15fd3d7f79412f
SafonovaNatalya <116429612+SafonovaNatalya@users.noreply.github.com>
2023-04-15 15:32:34 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,50 +1,52 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
-private:
- SYM <T> arr[size];
- int first, last, count;
-public:
- TQueue(): first(0), last(0), count(0){}
- bool isEmpty() const {
+ private:
+  SYM <T> arr[size];
+  int first, last, count;
+
+ public:
+  TQueue(): first(0), last(0), count(0){}
+  bool isEmpty() const {
   return count == 0;
- }
- bool isFull() const {
+  }
+  bool isFull() const {
   return count == size;
- }
- ~TQueue() {
+  }
+  ~TQueue() {
   delete[] arr;
- }
- void push(const T& value) {
+  }
+  void push(const T& value) {
   if (isFull())
-   throw std::string("Full!!");
+  throw std::string("Full!!");
   else {
-   count += 1;
-   int helper = last;
-   for (int i = first; i < last; i++) {
+  count += 1;
+  int helper = last;
+  for (int i = first; i < last; i++) {
     if (arr[i].prior < value.prior) {
      helper = i;
      break;
     }
-   }
-   for (int i = last; i < helper; i++) {
+  }
+  for (int i = last; i < helper; i++) {
     arr[i % size] = arr[(i - 1) % size];
-   }
-   arr[helper % size] = value;
-   last += 1;
   }
- }
- T& pop() {
+  arr[helper % size] = value;
+  last += 1;
+  }
+  }
+  T& pop() {
   if (isEmpty())
-   throw std::string("Empty!!");
+  throw std::string("Empty!!");
   else {
-   count -= 1;
-   return arr[first++ % size];
+  count -= 1;
+  return arr[first++ % size];
   }
- }
+  }
 };
 
 struct SYM {
